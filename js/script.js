fe005d71455944b1c0cf4d045e750c3b923d91f7
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-20 18:44:52 +0200

    final review

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+// Instance of Vue
 const app = new Vue({
     el: '#app',
     data: {
         title: 'Welcome Vue',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/555px-Vue.js_Logo_2.svg.png'
+        image: './img/Vue.png'
     }
 });
