31882722f2dfda34e5d3bf3763be56a209d106df
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-20 17:50:47 +0200

    add logic and style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Vue', Vue); //test
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Welcome Vue',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/555px-Vue.js_Logo_2.svg.png'
+    }
+});
